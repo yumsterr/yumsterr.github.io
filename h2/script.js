7216889d2ceacd855f01bafe5fa124b5ae89c4b5
Lina Vasylieva <Yumster@Yumster-mac.local>
2017-07-08 21:31:40 +0300

    responsive done

---
+++ h2/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#nav-icon2').addEventListener("click", function(e){
+    this.classList.toggle('open');
+    document.querySelector('.header-menu').classList.toggle('open');
+});
