ecf3bd4d4f4578dfa6bf3bf24ff16936b35ff7e4
Lina Vasylieva <Yumster@Yumster-mac.Dlink>
2017-07-12 10:45:22 +0300

    h3 partly done

---
+++ h3/script.js
@@ -0,0 +1,185 @@
+/**
+ * Created by Yumster on 7/10/17.
+ */
+
+fetch('https://api.myjson.com/bins/152f9j')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (responseData) {
+        let articles = Object.assign(responseData.data);
+        getAllTags(articles);
+        feedBuilder(articles);
+    })
+    .catch(alert);
+
+const newsParentNode = document.querySelector('.news-feed');
+
+let rebuildFeed = new Event('rebuild');
+
+let feedBuilder = (articles) => {
+    articles = sortItems(articles);
+    buildItems(10, 0, articles);
+    checkActiveTags();
+    loader('hide');
+    newsParentNode.addEventListener('rebuild', function(e){
+        loader('show');
+        newsParentNode.innerHTML = "";
+        articles = sortItems(articles);
+        buildItems(10, 0, articles);
+        checkActiveTags();
+        loader('hide');
+    })
+};
+
+let buildItems = (count, start, articles) => {
+    let cellProto = document.querySelector('.news-item.proto').innerHTML;
+    // const newsParentNode = document.querySelector('.news-feed');
+    for (let i = 0; i < 10; i++) {
+        let newChild = document.createElement('div');
+        const currentArticle = articles[i];
+
+        newChild.innerHTML = cellProto;
+        newChild.classList.add('news-item');
+        newsParentNode.appendChild(newChild);
+
+        let imgNode = newChild.querySelector('img');
+        let nameNode = newChild.querySelector('.name');
+        let dateNode = newChild.querySelector('.date');
+        let tagsNode = newChild.querySelector('.tags');
+        let descriptionNode = newChild.querySelector('.description');
+
+        if (currentArticle.image.length)
+            imgNode.setAttribute('src', currentArticle.image);
+
+        if (currentArticle.title.length)
+            nameNode.innerHTML = currentArticle.title;
+
+        if (currentArticle.createdAt.length) {
+            let dateFormat = new Date(currentArticle.createdAt);
+            dateNode.innerHTML = dateFormat.toLocaleDateString() + ' ' + dateFormat.toLocaleTimeString();
+        }
+
+        if (currentArticle.tags.length)
+            tagsBuilder(currentArticle.tags, tagsNode);
+
+        if (currentArticle.description.length)
+            descriptionNode.innerHTML = currentArticle.description;
+    }
+};
+
+let getAllTags = (articles) => {
+    let tags = articles.map(function (a) {
+        return a.tags;
+    });
+    let merged = [].concat.apply([], tags);
+    let unique = Array.from(new Set(merged))
+    let tagListBlock = document.getElementById('tagBlock');
+    tagsBuilder(unique, tagListBlock);
+};
+
+let tagsBuilder = (tagList, parent) => {
+    if (tagList.length) {
+        for (let i = 0; i < tagList.length; i++) {
+            let tag = tagList[i];
+            let newTag = document.createElement('a');
+            newTag.setAttribute('data-tag', tag);
+            newTag.setAttribute('href', '#');
+            newTag.innerHTML = '#' + tag;
+            parent.appendChild(newTag);
+            newTag.addEventListener('click', function (e) {
+                tagSort(this.getAttribute('data-tag'));
+            })
+        }
+    }
+    return true;
+};
+
+let tagSort = (tag) => {
+    let savedTags = JSON.parse(localStorage.getItem('tags'));
+    if (savedTags === null) {
+        savedTags = [];
+    }
+    let tagInd = savedTags.indexOf(tag);
+    if (tagInd === -1) {
+        savedTags.push(tag);
+    } else {
+        savedTags.splice(tagInd, 1);
+        let allTagNodes = document.querySelectorAll('a[data-tag=' + tag + ']');
+        for (let i = 0; i < allTagNodes.length; i++) {
+            allTagNodes[i].classList.remove("active");
+
+        }
+    }
+    localStorage.setItem('tags', JSON.stringify(savedTags));
+    checkActiveTags();
+    newsParentNode.dispatchEvent(rebuildFeed);
+};
+
+let checkActiveTags = () => {
+    let savedTags = JSON.parse(localStorage.getItem('tags'));
+    if (savedTags !== null) {
+        savedTags.forEach(function (item, i, arr) {
+            let allTagNodes = document.querySelectorAll('a[data-tag=' + item + ']');
+            for (let i = 0; i < allTagNodes.length; i++) {
+                if (!allTagNodes[i].classList.contains("active")) {
+                    allTagNodes[i].classList.add("active");
+                }
+
+            }
+        });
+    }
+};
+
+let sortItems = (articles) => {
+    let savedTags = JSON.parse(localStorage.getItem('tags'));
+    let sortedArticles = articles.sort(function (a, b) {
+        if (savedTags !== null) {
+            let equalTagsA = getEqualItemsCount(a.tags, savedTags);
+            let equalTagsB = getEqualItemsCount(b.tags, savedTags);
+            if (equalTagsA > equalTagsB) {
+                return -1;
+            }
+            if (equalTagsA < equalTagsB) {
+                return 1;
+            }
+        }
+        if (a.createdAt > b.createdAt) {
+            return 1;
+        }
+        if (a.createdAt < b.createdAt) {
+            return -1;
+        }
+
+        return 0;
+    });
+
+    return sortedArticles;
+};
+
+
+let getEqualItemsCount = (array1, array2) => {
+    let iterableArray = array1;
+    let searchInArray = array2;
+    let equalCount = 0;
+    if (array2.length < array1.length) {
+        iterableArray = array2;
+        searchInArray = array1;
+    }
+    for (let i = 0; i < iterableArray.length; i++) {
+        let currentTag = iterableArray[i];
+        if (searchInArray.indexOf(currentTag) !== -1) {
+            equalCount++;
+        }
+    }
+    return equalCount;
+};
+
+let loader = (state) => {
+    let loaderNode = document.querySelector('.loader');
+    if(state === 'show'){
+        loaderNode.classList.add('active');
+    } else if (state === 'hide') {
+        loaderNode.classList.remove('active');
+    }
+};
